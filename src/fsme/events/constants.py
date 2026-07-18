7dd2ec037c29701635af5d58da34d19c5608ac87
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-18 23:11:18 +0800

    Create src/fsme/events/constants.py

---
+++ src/fsme/events/constants.py
@@ -0,0 +1,17 @@
+# src/fsme/events/constants.py
+
+"""
+Constants used by the event subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_EVENT_PRIORITY = 50
+
+MAX_EVENT_DEPTH = 1024
+
+SYSTEM_EVENT_SOURCE = "engine"
+
+PLAYER_EVENT_SOURCE = "player"
+
+CARD_EVENT_SOURCE = "card"
