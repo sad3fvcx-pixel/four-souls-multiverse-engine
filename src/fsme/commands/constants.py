f816786cb04198610a838bf79e9543333e3b0294
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-18 23:26:35 +0800

    Create src/fsme/commands/constants.py

---
+++ src/fsme/commands/constants.py
@@ -0,0 +1,17 @@
+# src/fsme/commands/constants.py
+
+"""
+Constants used by the command subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_COMMAND_PRIORITY = 50
+
+MAX_COMMAND_DEPTH = 1024
+
+SYSTEM_COMMAND_SOURCE = "engine"
+
+PLAYER_COMMAND_SOURCE = "player"
+
+CARD_COMMAND_SOURCE = "card"
