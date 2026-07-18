2090012579771608f23154a0549812d9d270c83d
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-18 23:14:06 +0800

    Update __init__.py

--- src/fsme/events/__init__.py
+++ src/fsme/events/__init__.py
@@ -1,15 +1,69 @@
 # src/fsme/events/__init__.py
 
 """
-Event system for Four Souls Multiverse Engine.
+Event subsystem exports.
 """
 
-from .event import Event
 from .bus import EventBus
+from .constants import (
+    CARD_EVENT_SOURCE,
+    DEFAULT_EVENT_PRIORITY,
+    MAX_EVENT_DEPTH,
+    PLAYER_EVENT_SOURCE,
+    SYSTEM_EVENT_SOURCE,
+)
+from .context import EventContext
+from .dispatcher import EventDispatcher
+from .errors import (
+    EventDispatchError,
+    EventHandlerError,
+    EventQueueError,
+    EventRegistrationError,
+)
+from .event import Event
+from .filter import (
+    EventFilter,
+    accept_all,
+    active_only,
+    cancelled_only,
+)
+from .listener import EventListener
+from .priority import EventPriority
+from .queue import EventQueue
+from .result import EventResult
+from .subscription import EventSubscription
 from .types import EventType
+from .utils import (
+    create_event,
+    ensure_not_cancelled,
+    is_cancelled,
+)
 
 __all__ = [
     "Event",
     "EventBus",
+    "EventContext",
+    "EventDispatcher",
+    "EventFilter",
+    "EventListener",
+    "EventPriority",
+    "EventQueue",
+    "EventResult",
+    "EventSubscription",
     "EventType",
+    "EventDispatchError",
+    "EventHandlerError",
+    "EventRegistrationError",
+    "EventQueueError",
+    "accept_all",
+    "active_only",
+    "cancelled_only",
+    "create_event",
+    "ensure_not_cancelled",
+    "is_cancelled",
+    "DEFAULT_EVENT_PRIORITY",
+    "MAX_EVENT_DEPTH",
+    "SYSTEM_EVENT_SOURCE",
+    "PLAYER_EVENT_SOURCE",
+    "CARD_EVENT_SOURCE",
 ]
