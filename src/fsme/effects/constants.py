f276e11b21cf53e5a23577b76153fc637e76a01d
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-19 09:19:26 +0800

    Create src/fsme/effects/constants.py

---
+++ src/fsme/effects/constants.py
@@ -0,0 +1,19 @@
+# src/fsme/effects/constants.py
+
+"""
+Constants used by the effect subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_EFFECT_PRIORITY = 50
+
+SYSTEM_EFFECT_SOURCE = "system"
+
+PLAYER_EFFECT_SOURCE = "player"
+
+CARD_EFFECT_SOURCE = "card"
+
+TRIGGER_EFFECT_SOURCE = "trigger"
+
+REPLACEMENT_EFFECT_SOURCE = "replacement"
