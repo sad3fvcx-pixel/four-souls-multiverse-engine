631fa7100ad68118c26428d6ad0d3d89937b1c82
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-19 09:10:29 +0800

    Create src/fsme/stack/constants.py

---
+++ src/fsme/stack/constants.py
@@ -0,0 +1,19 @@
+# src/fsme/stack/constants.py
+
+"""
+Constants used by the stack subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_STACK_LIMIT = 1024
+
+SYSTEM_STACK_SOURCE = "system"
+
+PLAYER_STACK_SOURCE = "player"
+
+CARD_STACK_SOURCE = "card"
+
+TRIGGER_STACK_SOURCE = "trigger"
+
+REPLACEMENT_STACK_SOURCE = "replacement"
