176dc5e64b4c0085b42e7c857768edc5b2e0ca8c
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-19 10:08:09 +0800

    Create src/fsme/serialization/constants.py

---
+++ src/fsme/serialization/constants.py
@@ -0,0 +1,29 @@
+# src/fsme/serialization/constants.py
+
+"""
+Constants for the serialization subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_ENCODING = "utf-8"
+
+DEFAULT_INDENT = 4
+
+DEFAULT_FORMAT = "json"
+
+JSON_EXTENSION = ".json"
+
+SAVE_EXTENSION = ".fsme"
+
+REPLAY_EXTENSION = ".replay"
+
+SCHEMA_VERSION = 1
+
+DEFAULT_SAVE_DIRECTORY = "saves"
+
+DEFAULT_REPLAY_DIRECTORY = "replays"
+
+DEFAULT_EXPORT_DIRECTORY = "exports"
+
+MANIFEST_FILENAME = "manifest.json"
