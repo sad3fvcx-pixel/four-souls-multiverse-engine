788b7284a22390650351f918bec606fe96945b05
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-19 13:16:20 +0800

    Create src/fsme/config/constants.py

---
+++ src/fsme/config/constants.py
@@ -0,0 +1,29 @@
+# src/fsme/config/constants.py
+
+"""
+Constants for the configuration subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_CONFIG_DIRECTORY = "config"
+
+DEFAULT_CONFIG_FILENAME = "config.json"
+
+DEFAULT_PROFILE = "default"
+
+DEFAULT_ENCODING = "utf-8"
+
+DEFAULT_FORMAT = "json"
+
+DEFAULT_AUTO_SAVE = False
+
+DEFAULT_AUTO_LOAD = True
+
+DEFAULT_SCHEMA_VERSION = 1
+
+DEFAULT_NAMESPACE = "core"
+
+MAX_KEY_LENGTH = 128
+
+MAX_VALUE_LENGTH = 4096
