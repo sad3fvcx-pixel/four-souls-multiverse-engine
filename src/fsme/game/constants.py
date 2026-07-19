7121c60478d4c2fc7037624dc591c09019f7c54f
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-19 09:43:19 +0800

    Create src/fsme/game/constants.py

---
+++ src/fsme/game/constants.py
@@ -0,0 +1,19 @@
+# src/fsme/game/constants.py
+
+"""
+Constants used by the game subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_MAX_PLAYERS = 4
+
+DEFAULT_MIN_PLAYERS = 2
+
+DEFAULT_STARTING_COINS = 3
+
+DEFAULT_STARTING_LOOT = 3
+
+DEFAULT_STARTING_TREASURES = 1
+
+DEFAULT_MAX_TURNS = 10_000
