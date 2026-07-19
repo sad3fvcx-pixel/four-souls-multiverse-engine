4f1d0c56b66cd11d8e502e7eb6eb006cf21afc07
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-19 09:31:07 +0800

    Create src/fsme/cards/constants.py

---
+++ src/fsme/cards/constants.py
@@ -0,0 +1,19 @@
+# src/fsme/cards/constants.py
+
+"""
+Constants used by the card subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_HAND_LIMIT = 10
+
+DEFAULT_SHOP_SIZE = 2
+
+DEFAULT_MONSTER_SLOTS = 2
+
+DEFAULT_ROOM_SLOTS = 1
+
+DEFAULT_SOULS_TO_WIN = 4
+
+DEFAULT_MAX_COUNTERS = 999
