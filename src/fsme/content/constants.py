8fa87beb00eaf965540d5bdc7c25dccfb753c5dd
sad3fvcx-pixel <sad3fvcx@gmail.com>
2026-07-19 09:54:00 +0800

    Create src/fsme/content/constants.py

---
+++ src/fsme/content/constants.py
@@ -0,0 +1,35 @@
+# src/fsme/content/constants.py
+
+"""
+Constants used by the content subsystem.
+"""
+
+from __future__ import annotations
+
+DEFAULT_CONTENT_NAMESPACE = "core"
+
+DEFAULT_CONTENT_VERSION = "1.0.0"
+
+DEFAULT_ENCODING = "utf-8"
+
+CARD_FILE_EXTENSION = ".json"
+
+CONTENT_MANIFEST_NAME = "manifest.json"
+
+CONTENT_DIRECTORY_NAME = "content"
+
+CARDS_DIRECTORY_NAME = "cards"
+
+EFFECTS_DIRECTORY_NAME = "effects"
+
+ROOMS_DIRECTORY_NAME = "rooms"
+
+MONSTERS_DIRECTORY_NAME = "monsters"
+
+TREASURES_DIRECTORY_NAME = "treasures"
+
+LOOT_DIRECTORY_NAME = "loot"
+
+CHARACTERS_DIRECTORY_NAME = "characters"
+
+STARTING_ITEMS_DIRECTORY_NAME = "starting_items"
